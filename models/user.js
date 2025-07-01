c0e61a017df3d082e744063246a2e32e1bc4b651
Abhijit Roy <roya9435@gmail.com>
2025-07-01 23:56:02 +0530

    connected MongoDB Atlas

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 //MongoDB setup
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb://127.0.0.1:27017/authtestapp`);
+mongoose.connect(process.env.MONGO_URI);
 
 const userSchema = mongoose.Schema({
     username: String,
