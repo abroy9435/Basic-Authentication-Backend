60943ad2011bf00c68ca48ab52cf6b556d2e7261
Abhijit Roy <roya9435@gmail.com>
2025-07-02 11:30:19 +0530

    required mongoose in app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const express= require('express');
 const app= express();
 const userModel = require("./models/user")
+const mongoose = require('mongoose'); 
 
 const bcrypt = require('bcrypt');
 const jwt = require ('jsonwebtoken');
