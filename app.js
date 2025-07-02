795d33e9b7113783c201525651db160d055b0173
Abhijit Roy <roya9435@gmail.com>
2025-07-02 12:34:24 +0530

    added port setup for render deployment

--- app.js
+++ app.js
@@ -84,4 +84,9 @@ app.get("/logout", (req,res)=>{
     res.cookie("token", "");
     res.render('loggedout');
 })
-app.listen(3000)
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
